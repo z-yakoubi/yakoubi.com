880c6a4b0d8ea9f8f961b556a8ba4e1ab0ea56b5
z-yakoubi <za.yakoubi@gmail.com>
2019-08-23 18:04:08 +0100

    jour2

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,157 @@
 <template>
-  <div id="app">
-    <nav :class="{ active: show }" class="navbar-nav" id="js-responsive-nav">
-      <div class="row">
-        <div class="logo col-4">
-          <img alt="logo" src="../src/assets/logo.png" />
-        </div>
-        <div class="btn-header col-lg-8">
-        <button class="header-btn" v-if="show" key="on" @click="show = false">
-          open
-        </button>
-        <button class="header-btn" v-else key="off" @click="show = true">
-          close
-        </button>
-        </div>
-        <div class="nav-responsive" :class="{ active: show }" v-if="show">
-        <router-link to="/">Home</router-link><br>
-        <router-link to="/about">About</router-link><br>
-        <router-link to="/blog">blog</router-link><br>
-        </div>
+    <div id="app">
+        <nav :class="{ active: show }" class="navbar-nav" id="js-responsive-nav">
+            <div class="row">
+                <div class="logo col-4">
+                    <img alt="logo" src="../src/assets/logo.png"/>
+                </div>
+                <div class="btn-header col-lg-8">
+                    <button class="header-btn" v-if="show" key="on" @click="show = false">
+                        open
+                    </button>
+                    <button class="header-btn" v-else key="off" @click="show = true">
+                        close
+                    </button>
+                </div>
+                <div class="nav-responsive" :class="{ active: show }" v-if="show">
+                    <router-link to="/">Home</router-link>
+                    <br>
+                    <router-link to="/about">About</router-link>
+                    <br>
+                    <router-link to="/blog">blog</router-link>
+                    <br>
+                </div>
 
-        <div v-else class="col-lg-8 col-sm-12 ">
-        <router-link class="b" to="/">Home</router-link>
-        <router-link class="b" to="/about">About</router-link>
-        <router-link class="b" to="/blog">blog</router-link>
+                <div v-else class="col-lg-8 col-sm-12 ">
+                    <router-link class="b" to="/">Home</router-link>
+                    <router-link class="b" to="/about">About</router-link>
+                    <router-link class="b" to="/blog">blog</router-link>
+                </div>
+            </div>
+        </nav>
+        <div class="ban">
+            <div class="slogan">Présentation</div>
+            <img src="../src/assets/ban2.jpg">
         </div>
-      </div>
-    </nav>
-    <div class="ban">
-      <img src="../src/assets/banniere.jpg"/>
+        <router-view/>
+        <footer class="main-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <address>
+                            <a href="tel:+212602811500">(+212) 602 81 15 00</a>
+                            <br/>
+                            <a href="mailto:contact@fullwaveagency.com">contact@fullwaveagency.com</a>
+                        </address>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="tel:+33631009556">(+33) 631 00 95 56</a>
+                        <br/>
+                        <a href="mailto:contact@fullwaveagency.com">contact@fullwaveagency.com</a>
+                    </div>
+                    <div class="col-md-4">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fullwaveagency">Facebook</a>
+                            </li>
+                            <li>
+                                <a href="https://www.drupal.org/fullwave">Drupal</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="text-muted">Copyright 2019 © Fullwave Technology, All right reserved</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-    <router-view />
-
-    <footer class="main-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <address>
-              <a href="tel:+212602811500">(+212) 602 81 15 00</a>
-              <br />
-              <a href="mailto:contact@fullwaveagency.com">contact@fullwaveagency.com</a>
-            </address>
-          </div>
-          <div class="col-md-4">
-            <a href="tel:+33631009556">(+33) 631 00 95 56</a>
-            <br />
-            <a href="mailto:contact@fullwaveagency.com">contact@fullwaveagency.com</a>
-          </div>
-          <div class="col-md-4">
-            <ul>
-              <li>
-                <a href="https://www.facebook.com/fullwaveagency">Facebook</a>
-              </li>
-              <li>
-                <a href="https://www.drupal.org/fullwave">Drupal</a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-12">
-            <p class="text-muted">Copyright 2019 © Fullwave Technology, All right reserved</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </div>
 </template>
+
 <style lang="scss">
-  @import "scss/bootstrap";
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
-  .ban {
-    position: relative;
-    width: 100%;
-    img{
-      width: 100%
+    @import "scss/bootstrap";
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
     }
-  }
-  .navbar-nav {
-    background: #bbd5d5;
-    width: 100%;
-    padding: 30px 0 ;
-    display: block;
-    position: fixed;
-    z-index: 1;
-    text-align: right;
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      padding: 0 20px;
-      &.router-link-exact-active {
-        color: #42b983;
-      }
+    .slogan {
+        position: absolute;
+        top: 50%;
+        color: white;
+        font-size: 50px;
+        padding: 0 10%;
     }
-  }
-  .logo{text-align: left}
-  .btn-header{
-    display: none
-  }
-  .nav-responsive{
-    display: none;
-  }
-  @media only screen and (max-width: 768px)  {
-    .btn-header{
-      position: relative;
-      display: block;
-      float: right;
+    .navbar-nav {
+        background: rgba(255, 255, 255, 0.8);
+        width: 100%;
+        padding: 12px 40px;
+        display: block;
+        position: fixed;
+        z-index: 1;
+        text-align: right;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+            padding: 0 20px;
+
+            &.router-link-exact-active {
+                color: #1fa0c1;
+            }
+        }
     }
-    .b{
-      display: none
+    .logo {
+        text-align: left
+    }
+    .btn-header {
+        display: none
     }
     .nav-responsive {
-      display: block;
-      width: 100%;
-      text-align: right;
+        display: none;
     }
-  }
+    .main-footer{
+        padding: 30px 0;
+        background: #333;
+        color: #fff;
+        a{
+            color: white;
+        }
+    }
+    @media only screen and (max-width: 768px) {
+        .btn-header {
+            position: relative;
+            display: block;
+            float: right;
+        }
+        .b {
+            display: none
+        }
+        .nav-responsive {
+            display: block;
+            width: 100%;
+            text-align: right;
+        }
+    }
 </style>
 
 <script>
-  export default {
-    data() {
-      return {
-        show: false
-      }
-    },
-    methods : {
-      navresponsive: function() {
-        this.show = !this.show;
 
-      }
+    export default {
+        el: '#app',
+        data() {
+            return {
+                show: false,
+
+            }
+        },
+        methods: {
+          navresponsive: function () {
+            this.show = !this.show;
+
+          }
+        }
     }
-  }
+
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="home">
+
+    <select v-model="selected" multiple>
+      <option disabled value="">Choisissez</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>Sélectionné : {{ selected }}</span>
+
     <div class="article">
-      <h1>Article FULLWAVE</h1>
+      <h1>Articles</h1>
       <ul v-for="article in articles">
-        <li>{{ article.attributes.title }} </li>
+        <li> {{ article.attributes.title }} </li>
         <p v-if="article.attributes.body" v-html="article.attributes.body.value"></p>
       </ul>
+    </div>
+
+
+    <div class="wrapper">
+      <div class="image three"></div>
+
+      <transition name="bounce" appear>
+        <div class="txt" v-if="visible">
+        <h1>Article1</h1>
+        <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+      </transition>
+
+    </div>
+
+    <div class="wrapper wrapper2">
+      <div class="image two"></div>
+      <div class="image five"></div>
+      <div class="image six"></div>
     </div>
   </div>
 </template>
@@ -17,7 +50,9 @@ export default {
   name: "blog",
   data() {
     return {
-      articles: []
+      articles: [],
+      selected: '',
+      visible: true
     }
   },
 
@@ -37,5 +72,102 @@ export default {
 <style lang="scss">
   .article{
     position: relative;
+  }
+
+  .article{
+    padding: 30px;
+  }
+
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(100px, auto);
+    &.wrapper2{
+      padding-top: 20px;
+    }
+  }
+  .image{
+    opacity: 0.7;
+    &.two {
+      grid-column: 1 / 5;
+      grid-row: 1 / 4;
+      background: url("../assets/ban1.png");
+      background-size: cover;
+      transition: all 0.2s ease-in-out;
+      &:hover{ transform: scale(1.1);}
+    }
+    &.three {
+      grid-column: 1 / 3 ;
+      grid-row: 1 / 5;
+      background: url("../assets/dec.jpg");
+      background-size: cover;
+      transition: all 0.2s ease-in-out;
+      &:hover{ transform: scale(1.1);}
+    }
+    &.five {
+      grid-column: 1 / 4;
+      grid-row: 4 / 6;
+      background: coral;
+      transition: all 0.2s ease-in-out;
+
+    }
+    &.six {
+      grid-column: 4;
+      grid-row: 4 / 6;
+      background:deeppink;
+      -webkit-transform-origin: 50% 0;
+      transform-origin: 50% 0;
+      -webkit-animation: swinging 4.5s ease-in-out forwards infinite;
+      animation: swinging 4.5s ease-in-out forwards infinite;
+    }
+  }
+  .image:hover{
+    opacity: 1;
+  }
+  .txt{
+    grid-column: 3 / 5;
+    grid-row: 1 / 5;
+    text-align: justify;
+    padding: 55px;
+  }
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes swinging {
+    0% {
+      transform: rotate(10deg);
+    }
+    50% {
+      transform: rotate(-10deg);
+    }
+    100% {
+      transform: rotate(10deg);
+    }
+  }
+
+  @keyframes hue {
+    from {
+      filter: hue-rotate(0deg);
+    }
+
+    to {
+      filter: hue-rotate(-360deg);
+    }
   }
 </style>
